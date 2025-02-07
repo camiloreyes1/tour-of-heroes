53d0fceb49ce064ef9fb495c9cb0e04d42b2a112
Juan Reyes <camiloreyes1125@gmail.com>
2025-02-06 19:50:18 -0600

    list hero

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Dr Nice'},
+  { id: 13, name: 'Bombastic'},
+  { id: 14, name: 'Mr Fantastic'},
+  { id: 15, name: 'Iron Man'},
+  { id: 16, name: 'Spiderman'}
+];
